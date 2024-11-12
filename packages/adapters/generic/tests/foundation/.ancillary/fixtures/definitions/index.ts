75b22642715dbbdbacdd5a3c35e37d1b1bd11ba8
WhereJuly <where.july@googlemail.com>
2024-11-12 18:50:53 +0300

    [adapters/generic] Implement (core, tdd) [DCPLDOAS-17]: oas operations collection

--- packages/adapters/generic/tests/foundation/.ancillary/fixtures/definitions/index.ts
+++ packages/adapters/generic/tests/foundation/.ancillary/fixtures/definitions/index.ts
@@ -1,7 +1,10 @@
 'use strict';
 
+import { OpenAPIV3_1 } from 'openapi-types';
+
 import valid_json from './valid.json';
 import smallest_oas from './smallest.oas.json';
+import _petstore from './petstore.oas.json';
 import operations from './operations.json';
 
 export {
@@ -10,9 +13,12 @@ export {
     operations
 };
 
+export const petstore = _petstore as unknown as OpenAPIV3_1.Document;
+
 const index = {
     valid_json,
     smallest_oas,
+    petstore: petstore as OpenAPIV3_1.Document,
     operations
 };
 
