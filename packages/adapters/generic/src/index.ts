5a6b655d70f0fd3a4d6025ec3fe509fe063aeab5
WhereJuly <where.july@googlemail.com>
2024-11-13 16:29:49 +0300

    [adapters/generic] Update (icicdr): expose package public interfaces

--- packages/adapters/generic/src/index.ts
+++ packages/adapters/generic/src/index.ts
@@ -1,1 +1,8 @@
-console.log('here... 1, 2, 3...');
+'use strict';
+
+export { default as OASOperationsCollection } from './OASOperations.collection.js';
+export { default as OASOperationVO } from './OASOperation.valueobject.js';
+export { default as OASJSONDefinitionsRetrieveService } from './OASJSONDefinitionsRetrieve.service.js';
+export { default as OASDBCException } from './exceptions/ProgressingException.js';
+
+export { EHTTPVerb } from './types.js';  
