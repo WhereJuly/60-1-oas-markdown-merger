1603ed23715953248339f86021382df2dbf039b1
WhereJuly <where.july@googlemail.com>
2024-11-16 10:36:35 +0300

    [looks] Implement (view): links in operations list

--- packages/looks/src/views/content/OASOperationsList.view.vue
+++ packages/looks/src/views/content/OASOperationsList.view.vue
@@ -1,10 +1,15 @@
 <template>
 
     <aside>
-        <h3>OAS Operations List</h3>
+        <h3>Endpoints</h3>
         <ul>
             <li v-for="operation in vm.operations.items" v-bind:key="operation.operationID">
-                <span v-text="operation.operationID"></span>
+                <a v-bind:href="`operations/${operation.operationID}`">
+                    <dl class="looks-operations-list-link">
+                        <dt v-text="operation.summary"></dt>
+                        <dd v-text="operation.verb"></dd>
+                    </dl>
+                </a>
             </li>
         </ul>
     </aside>
