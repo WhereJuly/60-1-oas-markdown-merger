e914f45069665c12f880e8b0be43f3589ece8d88
WhereJuly <where.july@googlemail.com>
2024-11-19 14:03:14 +0300

    [looks] Implement (view): content types dedicated view

---
+++ packages/looks/src/views/content/OASOperationContentTypes.partial.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <!-- WRITE: The selector among content types w/my @dcoupld/vue-marked -->
+    <!-- WRITE: This will require the dedicated partial view -->
+
+    <select v-model="vm.selected">
+        <option v-for="type in content.types" v-bind:key="type" v-bind:value="type">
+            {{ type }}
+        </option>
+    </select>
+
+    <div v-text="content.findType(vm.selected)"></div>
+
+</template>
+
+<script lang="ts">
+import { reactive, type PropType } from 'vue';
+
+import type { OASContentsCollection } from '@dcoupld/oas-generic-adapter';
+
+export default {
+    props: {
+        content: { required: true, type: Object as PropType<OASContentsCollection> }
+    },
+    setup(props: { content: OASContentsCollection; }) {
+
+        const _default = props.content.types[0];
+        const vm = reactive({ selected: _default });
+
+        return { vm };
+    },
+};
+
+</script>
--- packages/looks/src/views/content/OASOperationSpecification.view.vue
+++ packages/looks/src/views/content/OASOperationSpecification.view.vue
@@ -11,23 +11,19 @@
     </dl>
 
     <p v-text="operation.description"></p>
+
+    <!-- REFACTOR: With Tags object .isEmpty getter -->
     <p v-if="hasTags(operation)">Tags: <span v-text="operation.tags.toString()"></span></p>
 
-    <section v-if="operation.body">
-    
-        <h3>Body</h3>
-        
-        <p v-text="(operation.body as OpenAPIV3_1.RequestBodyObject).description"></p>
-        <p v-if="(operation.body as OpenAPIV3_1.RequestBodyObject).required"><b>Required</b></p>
+    <section v-if="!operation.body.isEmpty">
 
-        <ul>
-            <!-- WRITE: The selector among content types w/my @dcoupld/vue-marked -->
-            <li v-for="[type, definitions] in requestContent(operation)">
-                <b v-text="type"></b>
-                <p v-text="definitions"></p>
-                <hr>
-            </li>
-        </ul>
+        <h3>Body</h3>
+
+        <p v-if="operation.body.required"><small>Required</small></p>
+
+        <p v-text="operation.body.description"></p>
+
+        <OASOperationContentTypes:partial v-bind:content="operation.body.content" />
 
     </section>
 
@@ -35,34 +31,31 @@
 
         <h3>Responses</h3>
 
-
-
     </section>
 
 </template>
 
 <script lang="ts">
-import { OASDBCException, type OASOperationVO } from '@dcoupld/oas-generic-adapter';
-import { OPERATIONS_COLLECTION } from '@src/ts/adapter/AdapterBootstrap.service';
-import type { OpenAPIV3_1 } from 'openapi-types';
 import { container } from 'tsyringe';
 
+import { OASDBCException, type OASOperationVO } from '@dcoupld/oas-generic-adapter';
+import { OPERATIONS_COLLECTION } from '@src/ts/adapter/AdapterBootstrap.service';
+import OASOperationContentTypePartial from './OASOperationContentTypes.partial.vue';
 
 export default {
     props: {
         operationID: { required: true, type: String }
     },
     components: {
+        'OASOperationContentTypes:partial': OASOperationContentTypePartial
     },
     setup(props: { operationID: string; }) {
-        // console.log(props.operationID);
 
+        // REFACTOR: Must be accessible from OASGenericAdapter.operations property.
+        // instead of AdapterBootstrapService
         const collection = container.resolve(OPERATIONS_COLLECTION);
 
-        // REFACTOR: Implement findByOperationID on OASOperationsCollection.
-        const operation = collection.items.find((operation: OASOperationVO) => {
-            return operation.operationID === props.operationID;
-        });
+        const operation = collection.findByOperationID(props.operationID);
 
         if (!operation) {
             throw new OASDBCException(`Unexpected missing operation with ID "${props.operationID}" in the definitions provided.`);
@@ -70,22 +63,9 @@ export default {
 
         const hasTags = (operation: OASOperationVO) => { return operation.tags && operation.tags.length > 0; };
 
-        // WRITE: get operation.body.contentItems(): get the key: value pairs of the content items. 
-        const requestContent = (operation: OASOperationVO): [string | null, unknown][] => {
-            // REFACTOR: In actual implementation to avoid redundant type casts.
-            return operation.body && (operation.body as OpenAPIV3_1.RequestBodyObject).content ?
-                Object.entries(((operation.body as OpenAPIV3_1.RequestBodyObject)).content) : [[null, null]];
-        };
-
-        const responseContent = (operation: OASOperationVO): [string | null, unknown][] => {
-            // REFACTOR: In actual implementation to avoid redundant type casts.
-            return operation.body && (operation.body as OpenAPIV3_1.RequestBodyObject).content ?
-                Object.entries(((operation.body as OpenAPIV3_1.RequestBodyObject)).content) : [[null, null]];
-        };
-
         console.dir(operation);
 
-        return { operationID: props.operationID, operation, hasTags, requestContent };
+        return { operationID: props.operationID, operation, hasTags };
     },
 };
 
