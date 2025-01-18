a037ac3a8aa1a1c3d4e8ffdeb3eeaa0d1eddd2f0
WhereJuly <where.july@googlemail.com>
2025-01-18 14:49:31 +0300

    [utilities/markdown-merger] Implement (design): oas markdown merger facade definitions retrieve
    DCPLDOAS-58

--- tests/foundation/unit/OASMarkdownMergerFacade.test.ts
+++ tests/foundation/unit/OASMarkdownMergerFacade.test.ts
@@ -8,12 +8,27 @@ import OASJSONDefinitionsRetrieveService from '@src/shared/OASJSONDefinitionsRet
 
 describe('OASMarkdownMergerFacadeTest', () => {
 
-    it('+static create() #1: Should create the expected OASMarkdownMergerFacade object', () => {
+    it('+static create(): Should create the expected OASMarkdownMergerFacade object', () => {
         const definitionsRetrieveService = new OASJSONDefinitionsRetrieveService();
 
         const actual = OASMarkdownMergerFacade.create(definitionsRetrieveService);
 
         expect(actual).toBeInstanceOf(OASMarkdownMergerFacade);
+        expect(actual.merge).toBeInstanceOf(Function);
+
     });
+
+    it('+merge() #1: Should load the definitions and assign them privately', () => {
+        const definitionsRetrieveService = new OASJSONDefinitionsRetrieveService();
+        const facade = OASMarkdownMergerFacade.create(definitionsRetrieveService);
+        const source = './tests/foundation/.ancillary/fixtures/definitions/petstore.oas.json';
+        const destinationFile = 'dummy';
+
+        facade.merge(source, destinationFile);
+
+        // WRITE: How to test the merge operation actually retrieved the spec?
+    });
+
+    // Assert: internal retrieve throws 
 
 });
