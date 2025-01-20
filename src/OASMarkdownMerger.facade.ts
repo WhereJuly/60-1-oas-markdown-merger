06a62ea81c8764f8cb482a0a6762300e948adbe3
WhereJuly <where.july@googlemail.com>
2025-01-20 10:40:05 +0300

    [utilities/markdown-merger] Implement (core): implement markdown merger facase
    happy case, DCPLDOAS-62

--- src/OASMarkdownMerger.facade.ts
+++ src/OASMarkdownMerger.facade.ts
@@ -1,21 +1,33 @@
 'use strict';
 
 import { OpenAPIV3_1 } from 'openapi-types';
+import path from 'path';
+import fs from 'fs';
+import { marked } from 'marked';
+import DOMPurify from 'isomorphic-dompurify';
 
 import OASJSONDefinitionsRetrieveService from '@src/shared/OASJSONDefinitionsRetrieve.service.js';
 import traverse from 'traverse';
+import OASDBCException from '@src/shared/OASDBCException.js';
+
+type TMergeableDescription = { path: string[]; description: string; };
+
+const MERGE_TAG_REGEX = /{% merge '.+' %}/;
 
 export default class OASMarkdownMergerFacade {
 
     #definitionsRetrieveService: OASJSONDefinitionsRetrieveService;
-    #definitions: OpenAPIV3_1.Document | {};
+    // #definitions: OpenAPIV3_1.Document | {};
+    #basePath: string;
 
     /**
      * NB: The class creation only available via `create` static method.
+     * WRITE: TDD with explicit basePath
      */
-    private constructor(definitionsRetrieveService: OASJSONDefinitionsRetrieveService) {
+    private constructor(definitionsRetrieveService: OASJSONDefinitionsRetrieveService, basePath?: string) {
         this.#definitionsRetrieveService = definitionsRetrieveService;
-        this.#definitions = {};
+        // this.#definitions = {};
+        this.#basePath = basePath ?? process.cwd();
     }
 
     public static create(definitionsRetrieveService: OASJSONDefinitionsRetrieveService): OASMarkdownMergerFacade {
@@ -23,42 +35,82 @@ export default class OASMarkdownMergerFacade {
     }
 
     public async merge(source: string, destinationFile: string): Promise<void> {
-        this.#definitions = await this.#definitionsRetrieveService.retrieve(source);
+        const definitions = await this.#definitionsRetrieveService.retrieve(source);
 
         // NB: Collect mergeable descriptions.
-        const descriptions = this.collectMergeableDescriptions(this.#definitions as OpenAPIV3_1.Document);
+        const descriptions = this.collectMergeableDescriptions(definitions);
 
         // NB: Replace merge tags with actual content.
-        descriptions.forEach(({ path, value }) => {
-            
-            // WRITE: Produce value to update the description field
-            // - Read the merged file, throw if not exist
-            // - check it is text, throw it is not
-            // - translate to HTML, return HTML.
-            
-            const html = 'file will be here';
-            const updatedValue = value.replace(/{% merge '.+' %}/, html);
-            traverse(this.#definitions).set(path, updatedValue);
+        descriptions.forEach(({ path, description }) => {
+            const html = this.produceHTML(description);
+            const updatedValue = description.replace(MERGE_TAG_REGEX, html);
+            traverse(definitions).set(path, updatedValue);
         });
 
-        console.dir(descriptions);
-        console.dir(this.#definitions);
-
-        // WRITE: Save the updated document to the destination file.
+        // WRITE: TDD Save the updated document to the destination file.
+        this.writeToDestinationFile(definitions, destinationFile);
     }
 
-    private collectMergeableDescriptions(definitions: OpenAPIV3_1.Document): { path: string[]; value: string; }[] {
-        const descriptions: { path: string[]; value: string; }[] = [];
-
+    private collectMergeableDescriptions(definitions: OpenAPIV3_1.Document): TMergeableDescription[] {
         const isMergeTag = (key: string | undefined, value: any): boolean => {
-            return key === 'description' && typeof value === 'string' && /{% merge '.+' %}/.test(value);
+            return key === 'description' && typeof value === 'string' && MERGE_TAG_REGEX.test(value);
         };
 
+        const descriptions: TMergeableDescription[] = [];
+
         traverse(definitions).forEach(function (node) {
-            if (isMergeTag(this.key, node)) { descriptions.push({ path: this.path, value: node }); }
+            if (isMergeTag(this.key, node)) { descriptions.push({ path: this.path, description: node }); }
         });
 
         return descriptions;
+    }
+
+    // WRITE: Produce the HTML value to update the description field with
+    // - Read the merged file, throw if not exist
+    // - check it is text, throw it is not
+    // - translate to HTML, return HTML.
+    private produceHTML(description: TMergeableDescription['description']): string {
+        console.log('description: ', description);
+
+        const isValidMarkdownFilename = (filename: string | null | undefined) => {
+            return filename && filename && /^[a-zA-Z0-9._/-]+$/.test(filename) && path.extname(filename) === '.md';
+        };
+
+        // NB: The filename must be relative to project root (cwd())
+        // WRITE: TDD 1) default basePath and 2) given basePath
+        const match = description.match(/{% merge ['"](.+?)['"] %}/);
+        const filename = match ? match[1] : null;
+
+        // WRITE: TDD invalid filename
+        if (!isValidMarkdownFilename(filename)) { throw new OASDBCException(`Invalid filename in "merge" tag given "${filename}".`); }
+
+        // WRITE: TDD non-existent file
+        const fullPath = path.resolve(this.#basePath, filename as string);
+        if (!fs.existsSync(fullPath)) { throw new OASDBCException(`The file "${fullPath}" does not exist.`); }
+
+        const markdown = fs.readFileSync(fullPath, 'utf-8');
+        const html = marked(markdown) as string;
+
+        // WRITE: TDD sanitize HTML
+        const sanitizedHTML = DOMPurify.sanitize(html);
+
+        return sanitizedHTML;
+    }
+
+    private writeToDestinationFile(definitions: OpenAPIV3_1.Document, destinationFile: string): void {
+        const isValidJsonFilePath = (filePath: string) => path.extname(filePath) === '.json' && fs.existsSync(path.dirname(filePath));
+
+        if (!isValidJsonFilePath(destinationFile)) {
+            throw new OASDBCException(`Invalid destination file path "${destinationFile}". The destination folder must exist. The file must be a JSON file (.json).`);
+        }
+
+        try {
+            fs.writeFileSync(destinationFile, JSON.stringify(definitions));
+        } catch (_error) {
+            const error = _error as Error;
+
+            throw new OASDBCException(`Could not stringify and write the updated definitions to the destination file. \n\rOriginal error message: ${error.message}`, error);
+        }
     }
 
 }
--- tests/foundation/unit/OASMarkdownMergerFacade.test.ts
+++ tests/foundation/unit/OASMarkdownMergerFacade.test.ts
@@ -1,12 +1,27 @@
 'use strict';
 
-import { describe, expect, it } from 'vitest';
+import { afterAll, afterEach, beforeEach, describe, expect, it } from 'vitest';
+
+import fs from 'fs';
 
 import OASMarkdownMergerFacade from '@src/OASMarkdownMerger.facade.js';
 
 import OASJSONDefinitionsRetrieveService from '@src/shared/OASJSONDefinitionsRetrieve.service.js';
 
+const tempDirectory = './tests/foundation/.ancillary/fixtures/.temp';
+
 describe('OASMarkdownMergerFacadeTest', () => {
+
+    // Ensure a clean state for written files before each test.
+    beforeEach(() => {
+        fs.existsSync(tempDirectory) && fs.rmSync(tempDirectory, { recursive: true, force: true });
+        fs.mkdirSync(tempDirectory);
+    });
+
+    // Clean up after tests
+    afterAll(() => {
+        fs.existsSync(tempDirectory) && fs.rmSync(tempDirectory, { recursive: true, force: true });
+    });
 
     it('+static create(): Should create the expected OASMarkdownMergerFacade object', () => {
         const definitionsRetrieveService = new OASJSONDefinitionsRetrieveService();
@@ -15,18 +30,27 @@ describe('OASMarkdownMergerFacadeTest', () => {
 
         expect(actual).toBeInstanceOf(OASMarkdownMergerFacade);
         expect(actual.merge).toBeInstanceOf(Function);
-
     });
 
-    it('+merge() #1: Should load the definitions and assign them privately', () => {
+    it('+merge() #1: Should merge the definitions and save the destination file', async () => {
+        // Arrange
+        const source = './tests/foundation/.ancillary/fixtures/definitions/petstore.oas.json';
+        const destinationFile = `${tempDirectory}/petstore-merged.oas.json`;
+        const expectedMarkdown = './tests/foundation/.ancillary/fixtures/markdown/simple.md';
+
         const definitionsRetrieveService = new OASJSONDefinitionsRetrieveService();
         const facade = OASMarkdownMergerFacade.create(definitionsRetrieveService);
-        const source = './tests/foundation/.ancillary/fixtures/definitions/petstore.oas.json';
-        const destinationFile = 'dummy';
 
-        facade.merge(source, destinationFile);
-
-        // WRITE: How to test the merge operation actually retrieved the spec?
+        // Assert
+        await facade.merge(source, destinationFile);
+        
+        const expected = fs.readFileSync(expectedMarkdown).toString('utf-8');
+        const merged = fs.readFileSync(destinationFile).toString('utf-8');
+        
+        const actual = merged.split(`<p>${expected}</p>`).length - 1;
+        
+        // Act
+        expect(actual).toEqual(3);
     });
 
     // Assert: internal retrieve throws 
