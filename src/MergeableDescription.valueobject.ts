6849667b7cf1c1048ad87fb24c2ca29c62949fda
WhereJuly <where.july@googlemail.com>
2025-01-20 15:25:36 +0300

    [utilities/markdown-merger] Add (docs): basic jsdoc
    DCPLDOAS-63

--- src/MergeableDescription.valueobject.ts
+++ src/MergeableDescription.valueobject.ts
@@ -9,11 +9,17 @@ import OASDBCException from '@src/shared/OASDBCException.js';
 
 const MERGE_TAG_REGEX = /{% merge '.+' %}/;
 
+/**
+ * Represents a value object to encapsulate merging descriptions in an OpenAPI document.
+ * 
+ * This class identifies descriptions containing special merge tags, validates associated
+ * markdown file references, and provides the markdown rendered as sanitized HTML.
+ */
 export default class MergeableDescriptionVO {
 
     private description: string;
     private mergingFileName: string;
-    
+
     public jsonPath: string[];
 
     private constructor(jsonPath: string[], description: string, mergingFileName: string) {
@@ -21,6 +27,35 @@ export default class MergeableDescriptionVO {
         this.description = description;
         this.mergingFileName = mergingFileName;
     }
+
+    /**
+     * Factory method to create an instance of `MergeableDescriptionVO` if the provided
+     * node contains a valid merge tag. If no valid merge tag is present, returns `null`.
+     * 
+     * @param {string | undefined} key - The key of the current JSON property being traversed.
+     * For the valid mergeable description expected to be `"description"`.
+     * 
+     * @param {string[]} jsonPath The JSON path segments as array for to the current node in the OpenAPI document.
+     * 
+     * @param {any} node The value of the current OpenAPI document property being processed.
+     * For `description` property may contain the merge tag.
+     * 
+     * @returns An instance of `MergeableDescriptionVO` if the node contains a valid merge tag; otherwise, `null`.
+     * 
+     * @throws {OASDBCException} If the merge tag contains an invalid filename.
+     * 
+     * @uses {@link MergeableDescriptionVO.constructor}
+     * 
+     * @used by {@link OASMarkdownMergerFacade.processMergeableDescriptions}
+     * 
+     * @example
+     * // Example usage within a traversal loop
+     * const key = 'description';
+     * const jsonPath = ['paths', '/example', 'get'];
+     * const node = "{% merge 'example.md' %}";
+     * 
+     * const mergeableDescription = MergeableDescriptionVO.create(key, jsonPath, node);
+     */
 
     public static create(key: string | undefined, jsonPath: string[], node: any): MergeableDescriptionVO | null {
         if (!this.isMergeTag(key, node)) { return null; }
@@ -34,6 +69,38 @@ export default class MergeableDescriptionVO {
         return new MergeableDescriptionVO(jsonPath, node, filename);
     }
 
+    /**
+     * Replaces the merge tag in the description with the sanitized HTML content
+     * rendered from the corresponding markdown file.
+     * 
+     * @param {string} mergeFileBasePath Th base path to locate the markdown file referenced in the merge tag.
+     * 
+     * @returns A string containing the updated description with the merge tag replaced by the rendered HTML.
+     * 
+     * @throws {OASDBCException} 
+     * - if the markdown file referenced in the merge tag does not exist
+     * - if any issues occur during file reading or HTML rendering.
+     * 
+     * @example
+     * // Example usage of the merged method
+     * const mergeableDescription = new MergeableDescriptionVO(
+     *   ['paths', '/example', 'get'],
+     *   "This is a {% merge 'example.md' %} description.",
+     *   'example.md'
+     * );
+     * 
+     * const mergeFileBasePath = './merges';
+     * const updatedDescription = mergeableDescription.merged(mergeFileBasePath);
+     * 
+     * console.log(updatedDescription);
+     * // Output: "This is a <sanitized HTML content> description."
+     */
+    public merged(mergeFileBasePath: string): string {
+        const html = this.renderHTML(mergeFileBasePath);
+
+        return this.description.replace(MERGE_TAG_REGEX, html);
+    }
+
     private renderHTML(mergeFileBasePath: string): string {
         const fullPath = path.resolve(mergeFileBasePath, this.mergingFileName);
         if (!fs.existsSync(fullPath)) { throw new OASDBCException(`The file "${fullPath}" does not exist.`); }
@@ -45,12 +112,6 @@ export default class MergeableDescriptionVO {
         const sanitizedHTML = DOMPurify.sanitize(html);
 
         return sanitizedHTML;
-    }
-
-    public merged(mergeFileBasePath: string): string {
-        const  html =  this.renderHTML(mergeFileBasePath);
-
-        return this.description.replace(MERGE_TAG_REGEX, html);
     }
 
     private static isMergeTag(key: string | undefined, value: any): boolean {
