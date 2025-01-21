03eca8a17e82c86925297ecb4c9a372b21e8e1f8
WhereJuly <where.july@googlemail.com>
2025-01-21 13:06:28 +0300

    [utilities/markdown-merger] Implement (usage): cli test

--- .usage/tests/UsageCLI.test.ts
+++ .usage/tests/UsageCLI.test.ts
@@ -3,11 +3,11 @@
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 
 import fs from 'fs';
-import { exec, execSync } from 'child_process';
+import { execSync } from 'child_process';
 
 const tempFolder = '../tests/foundation/.ancillary/fixtures/.temp';
 
-describe('CLI Application Usage', () => {
+describe('Usage CLI Application', () => {
 
     // Ensure a clean state for written files before each test.
     beforeEach(() => {
@@ -22,33 +22,20 @@ describe('CLI Application Usage', () => {
         fs.existsSync(tempFolder) && fs.rmSync(tempFolder, { recursive: true, force: true });
     });
 
-    it.only('Should successfully merge OAS file', () => {
+    it('merge #1: Should successfully merge markdown files from custom merges base directory', () => {
         const expectedMarkdown = '../tests/foundation/.ancillary/fixtures/markdown/simple.md';
 
-        const outputFile = `${tempFolder}/output.json`;
-        const output = execSync(`npx oas-markdown-merger --input ../tests/foundation/.ancillary/fixtures/definitions/petstore.oas.json --output ${outputFile}`).toString();
+        const destinationFile = `${tempFolder}/output.json`;
+        const output = execSync(`npx oas-markdown-merger --source ../tests/foundation/.ancillary/fixtures/definitions/petstore.oas.json --destination ${destinationFile} --merges-base ../`).toString();
 
         expect(output).toEqual(expect.stringContaining('Successfully merged to'));
 
         const expected = fs.readFileSync(expectedMarkdown).toString('utf-8');
-        const merged = fs.readFileSync(outputFile).toString('utf-8');
+        const merged = fs.readFileSync(destinationFile).toString('utf-8');
 
         const actual = merged.split(`<p>${expected}</p>`).length - 1;
 
         expect(actual).toEqual(3);
-    });
-
-    // eslint-disable-next-line @typescript-eslint/require-await
-    it('Should handle errors gracefully', async () => {
-        try {
-            exec('npx oas-markdown-merger --input input.json --output output.json');
-
-        } catch (_error) {
-            const error = _error as Error;
-
-            expect(error.message).toEqual(expect.stringContaining('ERROR'));
-            expect(error.message).toEqual(expect.stringContaining('input.json" does not exist'));
-        }
     });
 
 });
