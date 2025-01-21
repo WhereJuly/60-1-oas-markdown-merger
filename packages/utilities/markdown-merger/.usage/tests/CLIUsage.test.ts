d82ea5653e9979d7110863cbcd367e8b634206db
WhereJuly <where.july@googlemail.com>
2025-01-21 11:45:11 +0300

    [utilities/markdown-merger] Implement (usage, wip): basic cli test

---
+++ packages/utilities/markdown-merger/.usage/tests/CLIUsage.test.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+
+import fs from 'fs';
+import { exec, execSync } from 'child_process';
+
+const tempFolder = '../tests/foundation/.ancillary/fixtures/.temp';
+
+describe('CLI Application Usage', () => {
+
+    // Ensure a clean state for written files before each test.
+    beforeEach(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        fs.existsSync(tempFolder) && fs.rmSync(tempFolder, { recursive: true, force: true });
+        fs.mkdirSync(tempFolder);
+    });
+
+    // Clean up after tests
+    afterEach(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        fs.existsSync(tempFolder) && fs.rmSync(tempFolder, { recursive: true, force: true });
+    });
+
+    it.only('Should successfully merge OAS file', () => {
+        const expectedMarkdown = '../tests/foundation/.ancillary/fixtures/markdown/simple.md';
+
+        const outputFile = `${tempFolder}/output.json`;
+        const output = execSync(`npx oas-markdown-merger --input ../tests/foundation/.ancillary/fixtures/definitions/petstore.oas.json --output ${outputFile}`).toString();
+
+        expect(output).toEqual(expect.stringContaining('Successfully merged to'));
+
+        const expected = fs.readFileSync(expectedMarkdown).toString('utf-8');
+        const merged = fs.readFileSync(outputFile).toString('utf-8');
+
+        const actual = merged.split(`<p>${expected}</p>`).length - 1;
+
+        expect(actual).toEqual(3);
+    });
+
+    // eslint-disable-next-line @typescript-eslint/require-await
+    it('Should handle errors gracefully', async () => {
+        try {
+            exec('npx oas-markdown-merger --input input.json --output output.json');
+
+        } catch (_error) {
+            const error = _error as Error;
+
+            expect(error.message).toEqual(expect.stringContaining('ERROR'));
+            expect(error.message).toEqual(expect.stringContaining('input.json" does not exist'));
+        }
+    });
+
+});
