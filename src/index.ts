ea7232356b91cfa3d2b2022772df8237d90d3f7d
WhereJuly <where.july@googlemail.com>
2025-01-23 12:14:44 +0300

    [utilities/markdown-merger] Update (core): expose the more public inteface in package
    DCPLDOAS-70

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 'use strict';
 
 export { default as OASMarkdownMergerFacade } from './core/OASMarkdownMerger.facade.js';
+export { default as MergeableDescriptionVO } from './core/MergeableDescription.valueobject.js';
