d43ddbc82626114be5240a0e07788c36e95f2633
WhereJuly <where.july@googlemail.com>
2025-01-25 11:51:55 +0300

    [utilities/markdown-merger] Update (cli): output text, refactor missing arguments warning
    DCPLDOAS-77

--- packages/utilities/markdown-merger/src/cli/cli.ts
+++ packages/utilities/markdown-merger/src/cli/cli.ts
@@ -11,15 +11,28 @@ const pkg = createRequire(import.meta.url)("../../package.json") as PackageJson;
 
 const program = new Command();
 
-await program
+program
     .name(pkg.name)
     .version(pkg.version)
-    .description('Merges the (translated to HTML, sanitized) content of markdown files mentioned with "{% merge \'./docs/example.md\' %}" tags into respective OpenAPI JSON "description" fields.')
+    .description('Merges the content of markdown files (translated to HTML, sanitized) mentioned inside OpenAPI JSON source file "description" fields\' merge tags (namely e.g. "{% merge \'./docs/example.md\' %}") into the destination\'s respective "description" fields.')
     .usage("--source <file> --destination <file> [--merges-base <path>]")
-    .requiredOption('-s, --source <file>', 'Source OpenAPI definitions file.')
-    .requiredOption('-d, --destination <file>', 'Destination OpenAPI definitions file with descriptions merged.')
-    .option('-m, --merges-base <path>', 'The base path for files mentioned in the "merge" tags.')
+    .option('-s, --source <file>', 'Source OpenAPI definitions file.')
+    .option('-d, --destination <file>', 'Destination OpenAPI definitions file with descriptions merged (if any).')
+    .option('-m, --merges-base <path>', 'The base path for files mentioned in the "merge" tags. Defaults to the project root ("process.cwd()").')
+    .hook('preAction', (thisCommand, _actionCommand) => {
+        const args = thisCommand.opts();
+
+        if (!args.source || !args.destination) {
+            console.log(`\n[${colors.yellow}WARNING${colors.reset}] Provide the required "--source" and "--destination" arguments.\n`);
+
+            program.help();
+        }
+
+    })
     .action(async (options: { source: string; destination: string; mergesBase?: string; }) => {
+
+        console.log(`\n[${colors.yellow}WARNING${colors.reset}] Provide the required "--source" and "--destination" arguments.\n`);
+
         try {
             const facade = OASMarkdownMergerFacade.create(options.mergesBase);
             await facade.merge(options.source, options.destination);
@@ -32,5 +45,10 @@ await program
             program.error(`\n\r[${colors.red}ERROR${colors.reset}]: ${error.message}.`);
         }
     })
-    .showHelpAfterError()
-    .parseAsync(process.argv);
+    .showHelpAfterError();
+
+async function main() {
+    await program.parseAsync(process.argv);
+}
+
+await main();
